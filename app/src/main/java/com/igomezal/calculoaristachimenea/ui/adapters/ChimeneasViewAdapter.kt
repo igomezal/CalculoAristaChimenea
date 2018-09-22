0214b7760361ed5563f5229e69c46459ce3ee369
Iván Gómez Alonso <yvangomezalonso@gmail.com>
2018-09-22 21:57:06 +0200

    refactor: move adapter to their own package

---
+++ app/src/main/java/com/igomezal/calculoaristachimenea/ui/adapters/ChimeneasViewAdapter.kt
@@ -0,0 +1,37 @@
+package com.igomezal.calculoaristachimenea.ui.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.igomezal.calculoaristachimenea.R
+import com.igomezal.calculoaristachimenea.repository.entities.Chimenea
+
+class ChimeneasViewAdapter(private val chimeneas: List<Chimenea>) : RecyclerView.Adapter<ChimeneasViewAdapter.ViewHolder>() {
+    override fun getItemCount(): Int {
+        return chimeneas.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val chimenea: Chimenea = chimeneas[position]
+
+        viewHolder.chimeneaTitle.setText("""Lado a: ${chimenea.x} Lado b: ${chimenea.y} Altura: ${chimenea.height}""")
+        viewHolder.chimeneaResult.setText("""Resultado: ${chimenea.edge}""")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context: Context = parent.context
+        val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+
+        val chimeneaView: View = layoutInflater.inflate(R.layout.chimenea_view, parent, false)
+
+        return ViewHolder(chimeneaView)
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var chimeneaTitle: TextView = itemView.findViewById(R.id.chimenea_id)
+        var chimeneaResult: TextView = itemView.findViewById(R.id.chimenea_result)
+    }
+}
--- app/src/main/java/com/igomezal/calculoaristachimenea/ui/fragments/ListViewFragment.kt
+++ app/src/main/java/com/igomezal/calculoaristachimenea/ui/fragments/ListViewFragment.kt
@@ -1,7 +1,5 @@
 package com.igomezal.calculoaristachimenea.ui.fragments
 
-
-import android.content.Context
 import android.os.Bundle
 import android.support.design.bottomappbar.BottomAppBar
 import android.support.design.widget.FloatingActionButton
@@ -11,12 +9,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.igomezal.calculoaristachimenea.R
 import com.igomezal.calculoaristachimenea.repository.AppDatabase
-import com.igomezal.calculoaristachimenea.repository.entities.Chimenea
 import com.igomezal.calculoaristachimenea.ui.MainActivity
 import com.igomezal.calculoaristachimenea.ui.States
+import com.igomezal.calculoaristachimenea.ui.adapters.ChimeneasViewAdapter
 
 class ListViewFragment : Fragment() {
 
@@ -53,31 +50,4 @@ class ListViewFragment : Fragment() {
     }
 
 
-}
-
-class ChimeneasViewAdapter(private val chimeneas: List<Chimenea>) : RecyclerView.Adapter<ChimeneasViewAdapter.ViewHolder>() {
-    override fun getItemCount(): Int {
-        return chimeneas.size
-    }
-
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val chimenea: Chimenea = chimeneas[position]
-
-        viewHolder.chimeneaTitle.setText("""Lado a: ${chimenea.x} Lado b: ${chimenea.y} Altura: ${chimenea.height}""")
-        viewHolder.chimeneaResult.setText("""Resultado: ${chimenea.edge}""")
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val context: Context = parent.context
-        val layoutInflater: LayoutInflater = LayoutInflater.from(context)
-
-        val chimeneaView: View = layoutInflater.inflate(R.layout.chimenea_view, parent, false)
-
-        return ViewHolder(chimeneaView)
-    }
-
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var chimeneaTitle: TextView = itemView.findViewById(R.id.chimenea_id)
-        var chimeneaResult: TextView = itemView.findViewById(R.id.chimenea_result)
-    }
 }
